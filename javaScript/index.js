655bbe19f379936134ebcb530c371db4e7f91729
Daniel Cristofani <dcristofani@ciandt.com>
2022-04-14 13:47:57 -0300

    updates 14/04

--- javaScript/index.js
+++ javaScript/index.js
@@ -10,4 +10,29 @@ document.addEventListener('scroll', function() {
     }
 });
 
+function changePage(event) {
+    let pageReference = event.target.id;
+    let divLoader = document.getElementById('loader');
 
+    const promiseTimer = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+          }, 10000);
+    }) 
+
+    switch(pageReference) {
+        case 'indexPage':
+            console.log('indexPage');
+            divLoader.removeAttribute('class', 'display-loading-div');
+            promiseTimer
+            .then(() => {
+                window.location.href = 'index.html';
+            })
+            break;
+    }
+}
+
+async function setTimer(mili) {
+    setTimeout(() => {console.log('Passando aqui')}, mili)
+}
+// divLoader.setAttribute('class', 'display-loading-div');
